efa92b2458d27c700045b71de97295e24b20d46a
Thomas Galliker <thomas@bluewin.ch>
2023-05-23 06:37:33 +0200

    Add comments

--- NCrontab.Scheduler/ISchedulerOptions.cs
+++ NCrontab.Scheduler/ISchedulerOptions.cs
@@ -4,9 +4,13 @@ namespace NCrontab.Scheduler
     public interface ISchedulerOptions
     {
         /// <summary>
-        /// Specifies whether the scheduler uses local time or UTC for its operations.
+        /// Specifies whether the scheduler parses cron expressions as local time or UTC.
         /// Default is DateTimeKind.Utc.
         /// </summary>
+        /// <remarks>
+        /// Internal scheduler operations always run with UTC in order to have a
+        /// consistent behavior across daylight savings time changes.
+        /// Log messages are formatted with UTC timestamps too.</remarks>
         public DateTimeKind DateTimeKind { get; set; }
     }
 }
