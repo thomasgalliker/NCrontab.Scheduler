02dc7d15f4a7a2ad61a6aeffc8d7eda03bc59c8a
Thomas Galliker <thomas@bluewin.ch>
2023-08-10 21:40:53 +0200

    Mark properties virtual

--- NCrontab.Scheduler/LoggingOptions.cs
+++ NCrontab.Scheduler/LoggingOptions.cs
@@ -11,7 +11,7 @@ namespace NCrontab.Scheduler
             this.TaskIdFormatter = "B";
         }
 
-        public DateTimeKind DateTimeKind { get; set; }
+        public virtual DateTimeKind DateTimeKind { get; set; }
 
         /// <summary>
         /// Sets the formatting rule for a task identifier when written to the log output.
@@ -23,35 +23,12 @@ namespace NCrontab.Scheduler
         /// Since <see cref="ITask.Name"/> is optional,
         /// <see cref="ITask.Id"/> is used if <see cref="ITask.Name"/> is null or empty.
         /// </remarks>
-        public LogIdentifier LogIdentifier { get; set; }
+        public virtual LogIdentifier LogIdentifier { get; set; }
 
         /// <summary>
         /// Formatter used when logging <see cref="ITask.Id"/>.
         /// Default is <c>"B"</c>.
         /// </summary>
-        public string TaskIdFormatter { get; set; }
-    }
-
-    public enum LogIdentifier
-    {
-        /// <summary>
-        /// Use <see cref="ITask.Id"/> in the log messages.
-        /// </summary>
-        TaskId,
-
-        /// <summary>
-        /// Use <see cref="ITask.Name"/> in the log messages.
-        /// </summary>
-        TaskName,
-
-        /// <summary>
-        /// Use <see cref="ITask.Id"/> and <see cref="ITask.Name"/> (if available) in the log messages.
-        /// </summary>
-        TaskIdAndName,
-
-        /// <summary>
-        /// Use <see cref="ITask.Name"/> (if available) and <see cref="ITask.Id"/> in the log messages.
-        /// </summary>
-        TaskNameAndId,
+        public virtual string TaskIdFormatter { get; set; }
     }
 }
